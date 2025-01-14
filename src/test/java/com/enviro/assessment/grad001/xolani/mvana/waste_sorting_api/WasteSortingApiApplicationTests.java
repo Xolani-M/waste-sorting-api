d1b250ca5e749a9f3d76f74b2556c587990f3d10
sageMode <nathanielmvana@gmail.com>
2025-01-14 17:38:18 +0200

    Initial commit

---
+++ src/test/java/com/enviro/assessment/grad001/xolani/mvana/waste_sorting_api/WasteSortingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.xolani.mvana.waste_sorting_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasteSortingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
