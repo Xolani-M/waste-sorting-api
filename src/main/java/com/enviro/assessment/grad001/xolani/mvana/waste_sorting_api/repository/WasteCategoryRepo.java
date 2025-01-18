abcc3b4994ad85039325c69b8555d291cc242773
sageMode <nathanielmvana@gmail.com>
2025-01-18 12:30:42 +0200

    feat: add WasteCategoryRepository for JPA-based database operations
    - Created JPA repository interface for managing WasteCategory entities.
    - Enables CRUD operations on waste categories via database integration.

--- src/main/java/com/enviro/assessment/grad001/xolani/mvana/waste_sorting_api/repository/WasteCategoryRepo.java
+++ src/main/java/com/enviro/assessment/grad001/xolani/mvana/waste_sorting_api/repository/WasteCategoryRepo.java
@@ -1,4 +1,10 @@
 package com.enviro.assessment.grad001.xolani.mvana.waste_sorting_api.repository;
 
-public class WasteCategoryRepo {
+import com.enviro.assessment.grad001.xolani.mvana.waste_sorting_api.model.WasteCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WasteCategoryRepo extends JpaRepository<WasteCategory, Integer> {
+
 }
