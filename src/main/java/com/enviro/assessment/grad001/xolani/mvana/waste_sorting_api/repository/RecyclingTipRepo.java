e67d6bf05609eb5971ed1763f7ca75af6383c272
sageMode <nathanielmvana@gmail.com>
2025-01-19 17:14:14 +0200

    Added RecyclingTipRepo interface for managing RecyclingTip entities with JPA

--- src/main/java/com/enviro/assessment/grad001/xolani/mvana/waste_sorting_api/repository/RecyclingTipRepo.java
+++ src/main/java/com/enviro/assessment/grad001/xolani/mvana/waste_sorting_api/repository/RecyclingTipRepo.java
@@ -1,4 +1,9 @@
 package com.enviro.assessment.grad001.xolani.mvana.waste_sorting_api.repository;
 
-public class RecyclingTipRepo {
+import com.enviro.assessment.grad001.xolani.mvana.waste_sorting_api.model.RecyclingTip;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecyclingTipRepo extends JpaRepository<RecyclingTip, Integer> {
 }
