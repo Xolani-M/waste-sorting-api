1068e60adf89b1d1e7b6fbca19dbe84b912630b5
sageMode <nathanielmvana@gmail.com>
2025-01-31 07:05:42 +0200

    Add AuditingConfig class to enable JPA auditing

--- src/main/java/com/enviro/assessment/grad001/xolanimvana/config/AuditingConfig.java
+++ src/main/java/com/enviro/assessment/grad001/xolanimvana/config/AuditingConfig.java
@@ -1,4 +1,9 @@
 package com.enviro.assessment.grad001.xolanimvana.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing // Enables auditing annotations
 public class AuditingConfig {
 }
