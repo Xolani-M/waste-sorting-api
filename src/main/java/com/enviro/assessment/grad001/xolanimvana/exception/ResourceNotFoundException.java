80843b81a1ebc9801b62abfcf9de23470e6f78d0
sageMode <nathanielmvana@gmail.com>
2025-01-31 14:15:05 +0200

    Add GlobalExceptionHandler for error handling

---
+++ src/main/java/com/enviro/assessment/grad001/xolanimvana/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.enviro.assessment.grad001.xolanimvana.exception;
+
+public class WasteCategoryNotFoundException extends RuntimeException {
+    public WasteCategoryNotFoundException(String message) {
+        super(message);
+    }
+}
