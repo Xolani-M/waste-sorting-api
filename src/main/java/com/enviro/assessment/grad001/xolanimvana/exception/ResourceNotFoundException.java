0e5bb5ace710f1618ade21b7c7c48cb3f10ba6cb
sageMode <nathanielmvana@gmail.com>
2025-01-31 14:15:11 +0200

    Add ResourceNotFoundException class

--- src/main/java/com/enviro/assessment/grad001/xolanimvana/exception/ResourceNotFoundException.java
+++ src/main/java/com/enviro/assessment/grad001/xolanimvana/exception/ResourceNotFoundException.java
@@ -1,7 +1,7 @@
 package com.enviro.assessment.grad001.xolanimvana.exception;
 
-public class WasteCategoryNotFoundException extends RuntimeException {
-    public WasteCategoryNotFoundException(String message) {
-        super(message);
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String resource, Long id) {
+        super(resource + " not found with id: " + id);
     }
 }
